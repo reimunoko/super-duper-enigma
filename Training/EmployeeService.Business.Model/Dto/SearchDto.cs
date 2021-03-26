2bff2871f22890cdb2fa009e6daecf4cb5bbc6b9
Rei Murasaki <reimunoko@gmail.com>
2021-03-26 13:57:12 +0530

    Replaced SearchResults with AcmeGrid

--- Training/EmployeeService.Business.Model/Dto/SearchDto.cs
+++ Training/EmployeeService.Business.Model/Dto/SearchDto.cs
@@ -2,14 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EmployeeService.UI.Model;
 
 namespace EmployeeService.UI.Business.Model.Dto
 {
     public class SearchDto
     {
+        [AcmeGridColumn("Employee Name")]
         public string EmployeeName { get; set; }
+        [AcmeGridColumn("Designation")]
         public string Designation { get; set; }
+        [AcmeGridColumn("Department Name")]
         public string DepartmentName { get; set; }
+        [AcmeGridColumn("Location")]
         public string Location { get; set; }
 
     }
